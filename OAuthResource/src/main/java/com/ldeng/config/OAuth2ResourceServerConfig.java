47abc9dfd23906ff251376adb3485446001eb4fc
Le Deng <lede@Les-MacBook-Pro.local>
2016-08-25 15:41:28 -0400

    add UIPassword

--- OAuthResource/src/main/java/com/ldeng/config/OAuth2ResourceServerConfig.java
+++ OAuthResource/src/main/java/com/ldeng/config/OAuth2ResourceServerConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 import java.io.IOException;
---
+++ UIPassword/src/main/java/com/ldeng/UiPasswordApplication.java
@@ -0,0 +1,12 @@
+package com.ldeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiPasswordApplication.class, args);
+	}
+}
